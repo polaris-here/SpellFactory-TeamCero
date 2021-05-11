a94c5c0511d098b2069123fbe413e4cf4b9ed492
Adam Chance Lau <adclau@ucdavis.edu>
2021-05-10 21:54:53 -0700

    Start Factory Pattern

---
+++ Assets/Scripts/Spell.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Magic
+{
+    public interface ISpell
+{
+    void Cast();
+}
+}
--- Assets/Scripts/SpellFactory.cs
+++ Assets/Scripts/SpellFactory.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Magic;
 
 public class SpellFactory : MonoBehaviour
 {
+    [SerializeField] private GameObject Spell1;
+    [SerializeField] private GameObject Spell2;
+    [SerializeField] private GameObject Spell3;
+    [SerializeField] private GameObject DefaultSpawn;
+
+    public void Cast(GameObject Spell)
+    {
+        GameObject spell = Instantiate(Spell, this.DefaultSpawn.transform.position, Quaternion.identity);
+
+        Magic.ISpell spellController = spell.GetComponent<Magic.ISpell>();
+        spellController.Cast();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,19 @@ public class SpellFactory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Cast(this.Spell1);
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Cast(this.Spell2);
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            Cast(this.Spell3);
+        }
     }
 }
