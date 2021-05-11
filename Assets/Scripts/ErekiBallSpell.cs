a34790919d229a96fe157931696b3baf6e137d3c
Adam Chance Lau <adclau@ucdavis.edu>
2021-05-11 10:58:31 -0700

    Update ErekiBall spell effect
    Grows over time

--- Assets/Scripts/ErekiBallSpell.cs
+++ Assets/Scripts/ErekiBallSpell.cs
@@ -9,6 +9,7 @@ namespace Magic
             private Vector3 Force = new Vector3(500.0f, 0f, 0f);
             void ISpell.Cast(GameObject spell)
             {
+                this.transform.Rotate(new Vector3(0,90,0), Space.Self);
                 Rigidbody rb = spell.GetComponent<Rigidbody>();
                 rb.useGravity = false;
                 rb.AddForce(this.Force);
